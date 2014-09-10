575b9308b81131cdd3875acd380df2b59b701190
Tavonga Mudzana <41348@longroad.ac.uk>
2014-09-10 15:18:18 +0100

    2nd commit

---
+++ input.py
@@ -0,0 +1,10 @@
+#Tavonga Mudzana
+#10-09-2014
+#Exercise 1.2- Inputs
+print("hello world")
+#get a a name from the user and store it into a variable
+first_name=input("please enter your first name: ")
+#Printout the name stored in the varibale first name
+print(first name)
+#printout the name as part of a message
+prin ("hello {0}!".format(first_name))
