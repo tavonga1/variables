d1a1ae0555ff1fe2a48db47adf81ec2d925dd763
Tavonga Mudzana <41348@longroad.ac.uk>
2014-09-17 14:39:44 +0100

    class exercise

---
+++ 2int,divide.py
@@ -0,0 +1,17 @@
+#Tavonga Mudzana
+#17/09/14
+#revision exercise: task2
+
+print("hello please enter the three numbers you want to multiply then divide")
+
+n1=int(input("please enter your first number: "))
+n2=int(input("please enter your second number: "))
+n3=int(input("please enter the number you want to divide by: "))
+
+answer=(n1*n2)/n3
+
+print("this is your answer {0}".format(answer))
+
+             
+             
+             
---
+++ 4 integers.py
@@ -0,0 +1,16 @@
+#Tavonga Mudzana
+#17/09/14
+#class exercise
+
+#a program that adds 4 numbers
+print("Hello print put the 4numbers you want to add")
+number1=int(input("please enter your first number: "))
+number2=int(input("please enter your second number: "))
+number3=int(input("please enter your third number: "))
+number4=int(input("please enter your fourth number: "))
+#(inputs) asking the user for the 4 numbers
+answer=number1+number2+number3+number4
+print("this is your answer: {0}".format(answer))
+
+
+            
