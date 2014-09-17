4fb7e66f8501493bda9bff3b107ad8fb60974510
Tavonga Mudzana <41348@longroad.ac.uk>
2014-09-17 15:09:22 +0100

    revision sexercises

---
+++ task 6.3.py
@@ -0,0 +1,12 @@
+#Tavonga Mudzana
+#17/09/14
+#revission exercise: 6.3
+print(" hello please enter the numbers you wna to divide")
+number1=int(input("please enter your first number: "))
+number2=int(input("please enter your second number: "))
+
+
+mod_answer=number1%number2
+div_answer=number1//number2
+print("this is your your answer: {1} remainder: {0}".format(div_answer,mod_answer))
+
