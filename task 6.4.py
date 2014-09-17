e84e06cc7a53633fdb372919c696ec34b9e55f7a
Tavonga Mudzana <41348@longroad.ac.uk>
2014-09-17 15:15:42 +0100

    revision exercises

--- task 6.4.py
+++ task 6.4.py
@@ -5,8 +5,7 @@ print(" hello please enter the numbers you wna to divide")
 number1=int(input("please enter your first number: "))
 number2=int(input("please enter your second number: "))
 
-mod_answer=number1%number2
-div_answer=number15number2
+answer=number1//number2
 
-print("this is your your answer: {1}{0}".format(mod_answer,div_answer))
+print("this is your your answer: {0}".format(answer))
 
---
+++ volume.py
@@ -0,0 +1,11 @@
+print("this .........")
+
+length=float(input("please enter the length: "))
+width =float(input("please enter the width: "))
+height=float(input("please enhter the height(depth): "))
+
+volume=((length*width)*height)
+print("this is  your volume : {0}".format(volume))
+
+if(volume<10(print ("no"),volum>10(print ("no")
+   
